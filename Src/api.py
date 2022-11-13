b2eda1bd2b90ac5680fb840ef5a898f87b214f69
updraft <updraft.1970@gmail.com>
2022-11-13 11:28:51 +0900

    create: api main

--- Src/api.py
+++ Src/api.py
@@ -7,21 +7,51 @@ import xmltodict
 import json
 
 
-def CallApi(book_name):
+def APICall(book_name, cnt=5) -> dict:
     result = ""
-    URL = "https://iss.ndl.go.jp/api/sru"
+    URL = "https://iss.ndl.go.jp/api/opensearch"
+    params = {
+        'title': book_name,
+        "cnt": cnt}
+    res = requests.get(URL, params=params)
+    if res.status_code != 200:
+        print("ERROR HTTP GET")
+        return result
+    result = xmltodict.parse(res.text)
+    # result = res_json["rss"]["channel"]["item"][1]
     return result
 
 
+def APIValidationCheck(data) -> bool:
+    ERROR_MESSAGE = "ERROR Not Result"
+    if len(data) == 0:
+        print(ERROR_MESSAGE)
+        return False
+    if int(data["rss"]["channel"]["openSearch:totalResults"]) <= 1:
+        print(ERROR_MESSAGE)
+        return False
+    return True
+
+
+def APIShaping(book_data) -> dict:
+    item = book_data["rss"]["channel"]["item"][1]
+    response = {
+        "price": item['dcndl:price'],
+        "title": item['title'],
+        "publisher": item['dc:publisher'],
+        "date": item['dc:date']
+    }
+    return response
+
+
+def APIMain(book_name) -> dict:
+    result = {"status": False}
+    book_data = APICall(book_name)
+    if not APIValidationCheck(book_data):
+        return result
+    result["data"] = APIShaping(book_data)
+    return result
+
 if __name__ == "__main__":
-    url = 'https://iss.ndl.go.jp/api/opensearch'
-    params = {'title': 'AWSではじめるデータレイク',
-              "cnt": 5}
-    res = requests.get(url, params=params)
-    # print(params)
-    # pprint.pprint(res.text)
-    # xml_str = res.text
-    # element = ET.fromstring(xml_str)
-    # ET.dump(element)
-    element = xmltodict.parse(res.text)
-    pprint.pprint(element["rss"]["channel"]["item"][1])
+    result = APIMain("awsではじめるデータレイク ")
+    pprint.pprint(result)
