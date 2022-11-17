6ab88d1a23889bc56fc53de8260b679fdd9e3849
updraft <updraft.1970@gmail.com>
2022-11-18 08:27:37 +0900

    done

--- Src/main.py
+++ Src/main.py
@@ -1,12 +1,21 @@
 import os
 import api
 import pprint
-
+import markdown
+import env
 def cd():
     os.chdir(os.path.dirname(__file__))
 
 
 if __name__ == "__main__":
     cd()
-    pprint.pprint(api.APIMain("awsではじめるデータレイク"))
+    SEARCH_BOOK_NAME = "awsではじめるデータレイク"
+    # OUTPUT_FILE_PAHT = "output file path"
+    data = api.APIMain(SEARCH_BOOK_NAME)
+    if data["status"]:
+        print(markdown.MDMain(data["data"],
+              env.OUTPUT_FILE_PAHT + SEARCH_BOOK_NAME))
+    else:
+        print(data["status"], "Book Data Error")
+    # pprint.pprint(api.APIMain("awsではじめるデータレイク"))
     
--- Src/markdown.py
+++ Src/markdown.py
@@ -1,17 +1,26 @@
 import os
+import api
 
 
-def MDFileRead(path):
+def MDFileRead(path: str):
     with open(path, encoding="utf-8") as target:
         md_data = target.readlines()
     return md_data
 
 
+def MDFileWrite(write_data: list, path: str):
+    try:
+        with open(path, encoding="utf-8", mode="w") as target:
+            target.writelines(write_data)
+    except Exception as identifier:
+        print(identifier)
+        return False
+    return True
 def cd():
     os.chdir(os.path.dirname(__file__))
 
 
-def MDMain(data=""):
+def MDMain(data, output_path):
     cd()
     TEMPLATE_FILE_PATH = "../Doc/template.md"
     ADD_LIST = [
@@ -36,24 +45,55 @@ def MDMain(data=""):
             "key_name": "title"
         },
     ]
+    CHANGE_LIST = [
+        {"BulletPoints": ["price", "page"]},
+        {"ConvertPage": ["page"]},
+    ]
     template_data = MDFileRead(TEMPLATE_FILE_PATH)
-    test = {
-        'date': '2020.7',
-        'page': '377p',
-        'price': '2800円',
-        'publisher': 'テッキーメディア',
-        'title': 'AWSではじめるデータレイク = Data Lake starting with AWS : '
-        'クラウドによる統合型データリポジトリ構築入門 : クラウドを軸に統合データ基盤を作る'
-    }
-    for i in ADD_LIST:
-        headline, key_name = i.values()
-        template_data = add(template_data, headline, test[key_name])
-    print(*template_data)
-def add(data: list, headline: str, add_data: str):
-    add_index = data.index(headline + "\n") + 2
-    data.insert(add_index, add_data + "\n \n")
+    data = MDChangeMarkdownData(data, CHANGE_LIST)
+    template_data = MDAddMarkdownData(template_data, ADD_LIST,data)
+    return MDFileWrite(template_data, output_path)
+
+
+def MDChangeMarkdownData(data: list, change_list: list):
+    for i in change_list:
+        func_name = list(i.keys())[0]
+        data_list = list(i.values())[0]
+        if func_name == "BulletPoints":
+            for j in data_list:
+                data[j] = MDBulletPoints(data[j])
+        if func_name == "ConvertPage":
+            for j in data_list:
+                data[j] = MDConvertPage(data[j])
     return data
 
 
+def MDAddMarkdownData(template_data: list, add_list: list,data:list):
+    for i in add_list:
+        headline, key_name = i.values()
+        template_data = MDHeadlineAddData(
+            template_data, headline, data[key_name])
+    return template_data
+
+
+def MDHeadlineAddData(data: list, headline: str, add_data: str):
+    add_index = data.index(headline + "\n") + 2
+    new_line = "\n \n"
+    if add_data[0] == "-":
+        new_line = "\n"
+    data.insert(add_index, add_data + new_line)
+    return data
+
+
+def MDConvertPage(data):
+    return data.replace("p", "ページ")
+
+
+def MDBulletPoints(data):
+    return "- " + data
+
+
 if __name__ == "__main__":
-    MDMain()
+    data = api.APIMain("awsではじめるデータレイク")
+    # print(data["data"])
+    print(MDMain(data["data"]))
