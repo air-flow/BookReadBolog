d12b3499c9a7d442769f5969458c65c87258bc2e
updraft <updraft.1970@gmail.com>
2022-11-19 10:41:06 +0900

    create:test module

--- Src/main.py
+++ Src/main.py
@@ -7,6 +7,17 @@ def cd():
     os.chdir(os.path.dirname(__file__))
 
 
+def MainCall(SEARCH_BOOK_NAME):
+    cd()
+    # SEARCH_BOOK_NAME = "システム運用アンチパターン"
+    # OUTPUT_FILE_PAHT = "output file path"
+    data = api.APIMain(SEARCH_BOOK_NAME)
+    if data["status"]:
+        print(markdown.MDMain(data["data"],
+              env.OUTPUT_FILE_PAHT + SEARCH_BOOK_NAME))
+    else:
+        print(data["status"], "Book Data Error")
+
 if __name__ == "__main__":
     cd()
     SEARCH_BOOK_NAME = "システム運用アンチパターン"
---
+++ Src/test.py
@@ -0,0 +1,20 @@
+import api
+import pprint
+
+
+def NormalTest(book_name):
+    # main.MainCall(book_name)
+    pprint.pprint(api.APIMain(book_name))
+
+
+if __name__ == "__main__":
+    test_data = [
+        "システム運用アンチパターン",
+        "エンジニアリングマネージャーのしごと",
+        "ネットワークがよくわかる教科書",
+        "プロダクトマネジメントのすべて",
+        "事業をエンジニアリングする技術者たち",
+    ]
+    for i in test_data:
+        print("-" * 5 + " " + i + " " + "-" * 5)
+        NormalTest(i)
